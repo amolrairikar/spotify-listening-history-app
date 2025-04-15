c00ff40e8e5d9125befdfe07804fa53d7c50c4bf
= <arairikar1@gmail.com>
2025-04-15 10:18:53 -0500

    Updating shell script for integration tests

--- pipeline_scripts/run_integration_tests.sh
+++ pipeline_scripts/run_integration_tests.sh
@@ -9,20 +9,30 @@ docker compose up -d
 
 # Wait for LocalStack to be ready
 echo "Checking if LocalStack is ready..."
-# TODO: insert code to check if LocalStack is ready here
+for i in {1..60}; do
+  STATUS=$(curl -s http://localhost:4566/_localstack/health | jq -r '.services | to_entries[] | select(.value == "running") | .key' | wc -l)
+
+  if [ "$STATUS" -ge 1 ]; then
+    echo "LocalStack is ready."
+    break
+  else
+    echo "LocalStack not ready yet... ($i/60)"
+    sleep 1
+  fi
+done
+
+if [ "$STATUS" -lt 1 ]; then
+  echo "LocalStack did not become ready in time."
+  exit 1
+fi
 
 # Run integration tests
 echo "Running integration tests..."
-if ! pipenv run coverage run --source=src -m behave tests/integration; then
+if ! coverage run --source=src -m behave tests/integration; then
     echo "Integration tests failed!"
     exit 1
 fi
-mv .coverage .coverage.integration  # Move the coverage data to .coverage.integration
 
 # Stop LocalStack Docker container
 echo "Stopping LocalStack Docker container..."
 docker compose down
-
-# Delete the artifacts generated for zipping the Lambda function
-echo "Cleaning up artifacts..."
-# TODO: insert code to delete build directory and .zip file here
